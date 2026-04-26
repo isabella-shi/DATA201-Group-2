391819e5df3d339294cb37458c6954883d8976b6
Rachel Huang <rachel.huang823@gmail.com>
2026-04-26 16:25:08 -0700

    Add Rachel visualizations

--- flask/app.py
+++ flask/app.py
@@ -70,16 +70,35 @@ def index():
     ###################################
 
     sql7 = """
-    
+            SELECT 
+            DATE_FORMAT(`date`, '%Y-%m') AS month,
+            ROUND(SUM(ABS(CAST(REPLACE(REPLACE(amount, '$', ''), ',', '') AS DECIMAL(10,2)))), 2) AS total_spending
+            FROM Transactions_Sample
+            WHERE amount IS NOT NULL
+              AND amount != ''
+              AND `date` IS NOT NULL
+            GROUP BY DATE_FORMAT(`date`, '%Y-%m')
+            ORDER BY month;
     """
 
     sql8 = """
-    
+        SELECT 
+            mc.description AS category,
+            ROUND(SUM(ABS(CAST(REPLACE(REPLACE(t.amount, '$', ''), ',', '') AS DECIMAL(10,2)))), 2) AS total_spending
+        FROM Transactions_Sample t
+        JOIN MerchantCategories mc
+            ON t.mcc = mc.mcc_code
+        WHERE t.amount IS NOT NULL
+          AND t.amount != ''
+        GROUP BY mc.description
+        ORDER BY total_spending DESC
+        LIMIT 10;
     """
-
     return render_template("dashboard.html",
                            data1 = query(sql1),
-                           data2 = query(sql2)
+                           data2 = query(sql2),
+                           data7 = query(sql7),
+                           data8 = query(sql8)
                            )
 
 if __name__ == "__main__":
